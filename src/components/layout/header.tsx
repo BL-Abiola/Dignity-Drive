bf2244d85e4358df62866831e621bfb14c6cc251
Abdulganiyu Lukman <lukman4real001@gmail.com>
2025-12-12 16:11:51 +0000

    Make this entire section solid mode.  (_for element div_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -16,7 +16,7 @@ export function Header() {
   ];
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b bg-background">
       <div className="container flex h-14 items-center">
         <div className="mr-auto flex items-center">
           <Link href="/" className="mr-6 flex items-center space-x-2">
